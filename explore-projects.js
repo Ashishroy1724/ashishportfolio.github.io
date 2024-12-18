835199b0a904cbf8b81f243b2b687a88bb250bcf
Ashish Roy <114208768+Ashishroy1724@users.noreply.github.com>
2024-12-18 20:10:07 +0530

    New explore-projects.js

--- explore-projects.js
+++ explore-projects.js
@@ -1,31 +1,31 @@
 // Data for projects
 const projects = [
     {
-        name: "Project 1",
+        name: "Money Tracker",
         description: "An innovative app for managing personal finances with stunning UI/UX.",
         image: "Webstie Image - 3.png",
         link: "https://ashishroy1724.github.io/finance-manager.github.io/"
     },
     {
-        name: "Project 2",
+        name: "E-commerce Website",
         description: "A cutting-edge e-commerce website featuring seamless shopping experiences.",
         image: "Website Image 1.png",
         link: "https://fragranceofperfume.com/"
     },
     {
-        name: "Project 3",
+        name: "To-do-List",
         description: "A to-do list website made with the help HTML, CSS, Javascript.",
         image: "Website Image - 6.png",
         link: "https://ashishroy1724.github.io/to-do-list.github.io/"
     },
     {
-        name: "Project 4",
+        name: "Mortgage Broker",
         description: "A mortgage broker platform made using Wix Studio.",
         image: "Website Image - 7.png",
         link: "https://ashishproject78782.wixsite.com/mortgage-broker"
     },
     {
-        name: "Project 5",
+        name: "Figma Design",
         description: "A food delivery app designed with Figma to enhance user experience.",
         image: "Website Image - 8.png",
         link: "https://www.figma.com/design/zssyr9XmKmsraEsLCOFcv2/Food-Delivery-App?node-id=0-1&t=FihDTXcHy9CfDA4v-1"
@@ -55,3 +55,8 @@ function generateProjects() {
 
 // Call the function to generate the projects on page load
 document.addEventListener('DOMContentLoaded', generateProjects);
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+});
